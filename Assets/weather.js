b30d9feb34cec7d0f917016ec9e338373fafa79d
AdrenaRose <Charlenejparker81@gmail.com>
2023-04-02 13:50:52 -0400

    readded forecaset function

--- Assets/weather.js
+++ Assets/weather.js
@@ -47,12 +47,12 @@ function doClicky() {
 
 // document.getElementById("search-button").addEventListener("click", getWeather);
 
-// function getForecast() {
-//   let forecastUrl =
-//     "https://api.openweathermap.org/data/2.5/forecast?q=Richmond&units=imperial&appid=ac3f111a6c43635ae1941078ccac0b18";
-//   fetch(forecastUrl)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
-// }
+function getForecast() {
+  let forecastUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=Richmond&units=imperial&appid=ac3f111a6c43635ae1941078ccac0b18";
+  fetch(forecastUrl)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
 
-// getForecast()
+getForecast()
