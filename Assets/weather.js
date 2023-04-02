54fc2b9c9d5cc5b9b38bcf2578d7cc8be882fd71
AdrenaRose <Charlenejparker81@gmail.com>
2023-04-02 07:59:04 -0400

    fixed search button

--- Assets/weather.js
+++ Assets/weather.js
@@ -2,8 +2,6 @@ let apiKey = "ac3f111a6c43635ae1941078ccac0b18";
 let fetchButton = document.getElementById("search-button");
 let searchHistory = document.getElementById("search-history");
 
-document.getElementById("search-button").addEventListener("click", getApi);
-
 function getApi() {
   let requestUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=Richmond&units=imperial&appid=ac3f111a6c43635ae1941078ccac0b18";
@@ -12,4 +10,7 @@ function getApi() {
     .then((data) => console.log(data));
 }
 
+document.getElementById("search-button").addEventListener("click", getApi);
+
+console.log(document.getElementById("search-button"));
 // document.querySelector(".btn").addEventListener("click", getApi);
