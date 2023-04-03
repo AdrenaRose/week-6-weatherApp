873e7706fe33695c99819a5edfed36b2e408e277
AdrenaRose <Charlenejparker81@gmail.com>
2023-04-02 22:45:57 -0400

    testing displayForecast, loop not working

--- Assets/weather.js
+++ Assets/weather.js
@@ -20,9 +20,12 @@ function getWeather() {
       let { description, icon } = data.weather[0];
       let { temp, humidity } = data.main;
       let { speed } = data.wind;
-      document.getElementById("currentTemp").innerHTML = "Current Temperature in " + city + " is " + Math.round(temp) + "°F";
-      document.getElementById("currentWindspeed").innerHTML = "Windspeed is " + Math.round(speed) + " MPH";
-      document.getElementById("currentHumidity").innerHTML = "Humidity is " + humidity + "%";
+      document.getElementById("currentTemp").innerHTML =
+        "Current Temperature in " + city + " is " + Math.round(temp) + "°F";
+      document.getElementById("currentWindspeed").innerHTML =
+        "Windspeed is " + Math.round(speed) + " MPH";
+      document.getElementById("currentHumidity").innerHTML =
+        "Humidity is " + humidity + "%";
       document.getElementById("searchHistory").append(city);
       console.log(name, icon, description, temp, humidity, speed);
     });
@@ -31,14 +34,30 @@ function getWeather() {
 function clickEvent() {
   // document.getElementById("cityInput").value;
   getWeather();
+  getForecast();
 }
 
 function getForecast() {
+  let city = document.getElementById("cityInput").value;
   let forecastUrl =
-    "https://api.openweathermap.org/data/2.5/forecast?q=Richmond&units=imperial&appid=ac3f111a6c43635ae1941078ccac0b18";
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=imperial&appid=" +
+    apiKey;
   fetch(forecastUrl)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function displayForecast(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        let date = data.list[i].dt;
+        // let { description, icon } = data.list[i].weather[0];
+        // let { temp, humidity } = data.list[i].main;
+        // let { speed } = data.list[i].wind;
+        if (date !== dayjs().format()) {
+          console.log("Hello");
+        }
+      }
+    });
 }
-
-getForecast();
