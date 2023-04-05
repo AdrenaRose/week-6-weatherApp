bd4057cc48d5577a4f2f42d45be752e62f0b4a58
AdrenaRose <Charlenejparker81@gmail.com>
2023-04-05 18:11:24 -0400

    Working forecast functionality

--- Assets/weather.js
+++ Assets/weather.js
@@ -2,8 +2,10 @@ let apiKey = "ac3f111a6c43635ae1941078ccac0b18";
 let fetchButton = document.getElementById("search-button");
 let searchHistory = document.getElementById("search-history");
 
+//add clickEvent function to the search button - when user clicks search, execute clickEvent
 document.getElementById("search-button").addEventListener("click", clickEvent);
 
+//fetch current weather data to get today's weather and display it on the page
 function getWeather() {
   let city = document.getElementById("cityInput").value;
   let weatherUrl =
@@ -27,16 +29,10 @@ function getWeather() {
       document.getElementById("currentHumidity").innerHTML =
         "Humidity is " + humidity + "%";
       document.getElementById("searchHistory").append(city);
-      console.log(name, icon, description, temp, humidity, speed);
     });
 }
 
-function clickEvent() {
-  // document.getElementById("cityInput").value;
-  getWeather();
-  getForecast();
-}
-
+//fetch forecast data and call the getData function to display that data on the page
 function getForecast() {
   let city = document.getElementById("cityInput").value;
   let forecastUrl =
@@ -48,17 +44,70 @@ function getForecast() {
     .then(function (response) {
       return response.json();
     })
-    .then(function displayForecast(data) {
-      console.log(data);
-      for (var i = 0; i < data.list.length; i++) {
-        let date = data.list[i].dt;
-        let time = data.list[i].time;
-        let { description, icon } = data.list[i].weather[0];
-        let { temp, humidity } = data.list[i].main;
-        let { speed } = data.list[i].wind;
-        if (date !== dayjs().format() && time=== "12:00:00") {
-          console.log(description, icon);
-        }
-      }
+    .then(function (data) {
+      getData(
+        data,
+        dayjs().add(1, "day").subtract(12, "hour").unix(),
+        "description1",
+        "temp1",
+        "windspeed1",
+        "humidity1"
+      );
+      getData(
+        data,
+        dayjs().add(2, "day").subtract(12, "hour").unix(),
+        "description2",
+        "temp2",
+        "windspeed2",
+        "humidity2"
+      );
+      getData(
+        data,
+        dayjs().add(3, "day"),
+        "description3",
+        "temp3",
+        "windspeed3",
+        "humidity3"
+      );
+      getData(
+        data,
+        dayjs().add(4, "day"),
+        "description4",
+        "temp4",
+        "windspeed4",
+        "humidity4"
+      );
+      getData(
+        data,
+        dayjs().add(5, "day"),
+        "description5",
+        "temp5",
+        "windspeed5",
+        "humidity5"
+      );
     });
+}
+
+//create function for displaying weather on page
+function getData(data, dateToStopOn, descId, tempId, windId, humidId) {
+  for (var i = 0; i < data.list.length; i++) {
+    let date = data.list[i].dt;
+    if (date >= dateToStopOn) {
+      console.log(i);
+      let { description, icon } = data.list[i].weather[0];
+      let { temp, humidity } = data.list[i].main;
+      let { speed } = data.list[i].wind;
+      document.getElementById(descId).innerHTML = description;
+      document.getElementById(tempId).innerHTML = temp;
+      document.getElementById(windId).innerHTML = speed;
+      document.getElementById(humidId).innerHTML = humidity;
+      return;
+    }
+  }
+}
+
+//Defines the clickEvent function to call the getWeather and getForecast functions
+function clickEvent() {
+  getWeather();
+  getForecast();
 }
